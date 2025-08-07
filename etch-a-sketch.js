e612a23bd28df8a489f8dfcde651a8f21bdfd1de
smshaban117 <smshaban117@gmail.com>
2025-08-07 15:46:39 +0000

    Finished the final prompt loop and completed the project

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,10 @@
 const container = document.querySelector("#container")
 
 function gridmaker(columns, rows){
+const blockSize = 50
+container.style.width = `${rows*blockSize}px`
+container.style.height = `${columns*blockSize}px`
+
 for (let row = 0; row < rows; row++){
 for (let column = 0; column < columns; column++)
 { 
@@ -8,16 +12,33 @@ const block = document.createElement("div")
 block.className = "blocks"
 container.appendChild(block)
 
-}
+
 
 }
 
 }
 
-gridmaker(16,16)
+}
 
-const blocks = document.querySelectorAll(".blocks")
 
+
+
+
+
+
+const gridsize = document.querySelector("#gridsize")
+
+
+gridsize.addEventListener("click",()=>{                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+    let sizer = prompt("Enter a gridsize")
+    while (sizer >= 100 || sizer === "") {sizer = prompt("It should be below 100, Try again")}
+    container.innerHTML="";
+    gridmaker(sizer,sizer)
+
+    const blocks = document.querySelectorAll(".blocks")
 blocks.forEach(block=> block.addEventListener("mouseover",()=>{
     block.style.backgroundColor = "blue"
 }))
+})
+
+
